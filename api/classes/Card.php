3bee94229b9c3de6feaa0fc0ae53a61b8006d3e8
Danielly Santos <dsa.danielly15@gmail.com>
2023-01-11 15:09:42 -0300

    Update Card.php

--- api/classes/Card.php
+++ api/classes/Card.php
@@ -3,14 +3,15 @@ require_once "./connectDB.php";
 
 	class Card
 	{
-
+		//http://localhost/api_rest_enigma_tempo/api/card/getById/id
 		public function getById($idCard){
-      $id = $idCard[0];
+			$id = $idCard[0];
       $sql = "SELECT * FROM card WHERE id = $id";
       echo ($sql);
       return executeQuery($sql);
     }
-
+		
+		//http://localhost/api_rest_enigma_tempo/api/card/getAll
 		public function getAll()
 		{
 			$sql = "SELECT * FROM card ORDER BY id ASC";
